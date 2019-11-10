b551824f8283b5c1c029c86260de4e1a2e10a872
Josh Dick <josh@joshdick.net>
2019-11-10 18:55:28 -0500

    Attempt to use TMPDIR for temporary directory; fix swapped comment ordering

--- dntw.sh
+++ dntw.sh
@@ -34,12 +34,12 @@ function dntw_edit () {
       >&2 echo "dntw error: Could not determine tmux window ID!"
       return 1
     fi
-    local NVIM_LISTEN_ADDRESS="/tmp/dntw_${DNTW_ID}_${TMUX_WINDOW_ID}"
+    local NVIM_LISTEN_ADDRESS="/${TMPDIR-/tmp/}dntw_${DNTW_ID}_${TMUX_WINDOW_ID}"
     if [ $(nvr --serverlist | grep -ic "$NVIM_LISTEN_ADDRESS") -eq 1 ]; then
-      # A dedicated nvim is not yet running for this tmux window, start one
+      # A dedicated nvim is already running for this tmux window; reuse it
       NVR_CMD="$DNTW_NVIM_CMD" nvr --servername "$NVIM_LISTEN_ADDRESS" "$@"
     else
-      # A dedicated nvim is already running for this tmux window; reuse it
+      # A dedicated nvim is not yet running for this tmux window, start one
       NVIM_LISTEN_ADDRESS="$NVIM_LISTEN_ADDRESS" "$DNTW_NVIM_CMD" "$@"
     fi
   else
